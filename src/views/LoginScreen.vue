3b9a9120373df125d797b7380a37dc0f7ae3341a
HITALO VINICIUS RODRIGUES FROTA <hitalo.frota@tallos.intra.br>
2023-07-24 18:25:56 -0300

    feature-create screens

---
+++ src/views/LoginScreen.vue
@@ -0,0 +1,10 @@
+<template>
+    <p>Login</p>
+    <form>
+        <Label>Email</Label>
+        <input type="text" />
+        <Label>Senha</Label>
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+    </form>
+</template>

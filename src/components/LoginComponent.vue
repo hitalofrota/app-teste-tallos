1155fe922288c0372dcaf6e100f087c79bad659a
HITALO VINICIUS RODRIGUES FROTA <hitalo.frota@tallos.intra.br>
2023-07-24 23:02:49 -0300

    screens complete

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,26 +1,60 @@
 <template>
     <div>
-        <div>
-                <h1>Login</h1>
-            <form>
-                <label>Qual o seu login?</label>
-                <input type="text"/>
-                <!-- <input type="text" v-model="login" /> -->
-                <label>Qual a sua senha?</label>
-                <input type="text"/>
-                <!-- <input type="password" v-model="password" /> -->
-                <button type="submit">Entrar</button>
-            </form>
+        <div class="col-11">
+        <div class="row vh-100 d-flex align-items-center">
+            <div class="col-7">
+            </div>
+            <div class="col-5">
+                <h1 class="h5 mt-5">Faça o seu login</h1>
+                <div>
+                    <form>
+                    <div class="col-5">
+                        <input type="text" class="form-control mt-2" placeholder="Login" v-model="login">
+                    </div>
+                    <div class="col-5">
+                        <input type="text" class="form-control mt-2" placeholder="Senha" v-model="password">
+                    </div>
+                    <div>
+                        <button type="submit" class="btn btn-primary mt-3 buttonStyle" @click="exibirValor">Entrar</button>
+                    </div>
+                    </form>
+                </div>
+                <div>
+                    <p class="mt-5">Você ainda não possui cadastro?</p>
+                    <div>
+                        <router-link to="/register" class="btn btn-success buttonStyle">Cadastro</router-link>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-            <p>Ainda não possui cadastro?</p>
-            <button>Cadastre-se</button>
-        </div>
+    </div>
+        
     </div>
 </template>
 
 <script>
+
     export default {
-        name: 'LoginComponent'
+        name: 'LoginComponent',
+
+        data() {
+            return {
+                login: '',
+                password: ''
+            }
+        },
+
+        methods: {
+            exibirValor() {
+            console.log(this.login)
+            console.log(this.password)
+        },
+  },
     }
 </script>
+
+<style>
+.buttonStyle{
+    width: 150px !important;
+}
+</style>
--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -1,28 +1,72 @@
 <template>
-    <div>
-        <div>
-                <h1>Faça o seu cadastro</h1>
-            <form>
-                <label>Login</label>
-                <input type="text"/>
-                <!-- <input type="text" v-model="login" /> -->
-                <label>Email</label>
-                <input type="text"/>
-                <!-- <input type="password" v-model="password" /> -->
-                <label>Senha</label>
-                <input type="text"/>
-                <!-- <input type="password" v-model="password" /> -->
-                <label>Confirme sua senha</label>
-                <input type="text"/>
-                <!-- <input type="password" v-model="password" /> -->
-                <button type="submit">Registrar</button>
-            </form>
+    <div class="col-11">
+        <div class="row vh-90 d-flex align-items-center">
+            <div class="col-6">
+            </div>
+            <div class="col-5 ">
+                <h1 class="h2 mt-5">Seja bem vindo ao sistema de cadastro de funcionários</h1>
+                <h1 class="h5 mt-5">Faça o seu cadastro</h1>
+                <div>
+                    <form>
+                    <div>
+                        <label>Login</label>
+                        <input type="text" class="form-control mt-2" v-model="login">
+                    </div>
+                    <div>
+                        <label>Email</label>
+                        <input type="text" class="form-control mt-2" v-model="email">
+                    </div>
+                    <div>
+                        <label>Senha</label>
+                        <input type="text" class="form-control mt-2" v-model="password1">
+                    </div>
+                    <div>
+                        <label>Confirme sua senha</label>
+                        <input type="text" class="form-control mt-2" v-model="password2">
+                    </div>
+                    <div class="d-flex justify-content-center">
+                    <div class="buttonSpace">
+                        <button type="submit" class="btn btn-primary mt-2 buttonStyle" @click="exibirValor">Registrar</button>
+                    </div>
+                    <div class="buttonSpace">
+                        <router-link to="/" class="btn btn-success mt-2 buttonStyle">Voltar ao Início</router-link>
+                    </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'RegisterComponent'
+        name: 'RegisterComponent',
+
+        data() {
+            return {
+                login: '',
+                email: '',
+                password1: '',
+                password2: ''
+            }
+        },
+
+        methods: {
+            exibirValor() {
+            if(this.password1 != this.password2){
+                alert("A confirmação de senha não é igual a senha")
+            }
+        },
+        }
     }
 </script>
+
+<style>
+.buttonSpace{
+    margin: 10px;
+}
+.buttonStyle{
+    width: 150px !important;
+}
+</style>
